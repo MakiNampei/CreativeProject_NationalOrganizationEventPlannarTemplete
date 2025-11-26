dd9b1dad9c800cf3d6c91d35786767babfa1fa5b
iw380 <billy.z@wustl.edu>
2025-11-25 20:56:26 -0600

    backend

--- backend/server.js
+++ backend/server.js
@@ -29,16 +29,10 @@ app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/requests', require('./routes/requestRoutes'));
 
 
-// -----------------------------------------
-// DEPLOYMENT: Serve Frontend Static Files
-// -----------------------------------------
-// Point this to where you will put the 'build' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
-// -----------------------------------------
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
